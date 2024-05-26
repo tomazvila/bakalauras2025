d66406639a0451306b8b58e73653f57e8dc1ceb9
tomazvila <tomazvila@outlook.com>
2024-05-27 02:36:48 +0300

    init

---
+++ chess2tts/src/test/scala/la/tomazvi/PekkoQuickstartSpec.scala
@@ -0,0 +1,25 @@
+//#full-example
+package la.tomazvi
+
+import org.apache.pekko.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import la.tomazvi.Greeter.Greet
+import la.tomazvi.Greeter.Greeted
+import org.scalatest.wordspec.AnyWordSpecLike
+
+//#definition
+class PekkoQuickstartSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+//#definition
+
+  "A Greeter" must {
+    //#test
+    "reply to greeted" in {
+      val replyProbe = createTestProbe[Greeted]()
+      val underTest = spawn(Greeter())
+      underTest ! Greet("Santa", replyProbe.ref)
+      replyProbe.expectMessage(Greeted("Santa", underTest.ref))
+    }
+    //#test
+  }
+
+}
+//#full-example
