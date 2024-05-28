7a1fb208912de9a5adb9b941f72a0642562c7d62
tomazvila <tomazvila@outlook.com>
2024-05-28 22:15:16 +0300

    Leave only main actor

--- chess2tts/src/main/scala/la/tomazvi/PekkoQuickstart.scala
+++ chess2tts/src/main/scala/la/tomazvi/PekkoQuickstart.scala
@@ -6,35 +6,7 @@ import org.apache.pekko.actor.typed.Behavior
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
 import la.tomazvi.GreeterMain.SayHello
 
-object Greeter {
-  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
-  final case class Greeted(whom: String, from: ActorRef[Greet])
 
-  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
-    context.log.info("Hello {}!", message.whom)
-    message.replyTo ! Greeted(message.whom, context.self)
-    Behaviors.same
-  }
-}
-
-object GreeterBot {
-
-  def apply(max: Int): Behavior[Greeter.Greeted] = {
-    bot(0, max)
-  }
-
-  private def bot(greetingCounter: Int, max: Int): Behavior[Greeter.Greeted] =
-    Behaviors.receive { (context, message) =>
-      val n = greetingCounter + 1
-      context.log.info("Greeting {} for {}", n, message.whom)
-      if (n == max) {
-        Behaviors.stopped
-      } else {
-        message.from ! Greeter.Greet(message.whom, context.self)
-        bot(n, max)
-      }
-    }
-}
 
 object GreeterMain {
 
