f3ce151cb8fe16c3f399086517d7ab9595dfb21e
tomazvila <tomazvila@outlook.com>
2024-05-28 22:35:19 +0300

    Read iris data into DenseMatrix

--- chess2tts/src/main/scala/la/tomazvi/PekkoQuickstart.scala
+++ chess2tts/src/main/scala/la/tomazvi/PekkoQuickstart.scala
@@ -4,33 +4,48 @@ import org.apache.pekko.actor.typed.ActorRef
 import org.apache.pekko.actor.typed.ActorSystem
 import org.apache.pekko.actor.typed.Behavior
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import breeze.linalg.DenseMatrix
 import la.tomazvi.IrisReaderActor._
 import scala.io.Source
+
 
 object IrisReaderActor {
   sealed trait Command
   final object ReadIrisXData extends Command
   final object ReadIrisYData extends Command
+  
+  sealed trait Response
+  final case class IrisXData(dta: DenseMatrx[Double]) extends Response
+  final case class IrisYData(tgt: DenseMatrx[Int]) extends Response
 
   def apply(): Behavior[Command] =
     Behaviors.receiveMessage[Command] { 
       case ReadIrisXData => 
-        readData("iris_x.txt")
+        val data = readData("iris_x.txt")
+        val dta = parseIrisX(data)
         Behaviors.same
       case ReadIrisYData =>
-        readData("iris_y.txt")
+        val data = readData("iris_y.txt")
+        val tgt = parseIrisY(data)
         Behaviors.same
     }
 
-  private def readData(fileName: String): Unit = {
+  private def readData(fileName: String): List[String] = {
     val source = Source.fromResource(fileName)
     try {
-      val lines = source.getLines.toList
-      // Process the lines as needed
-      lines.foreach(println)
+      val lines: List[String] = source.getLines.toList
+      lines
     } finally {
       source.close()
     }
+  }
+
+  private def parseIrisX(lines: List[String]): DenseMatrix[Double] = {
+    DenseMatrix(lines.map(_.split(" ").map(_.toDouble)): _*)
+  }
+
+  private def parseIrisY(lines: List[String]): DenseMatrix[Int] = {
+    DenseMatrix(lines.map(_.split(" ").map(_.toInt)): _*)
   }
 }
 
